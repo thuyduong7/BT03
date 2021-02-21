e266a153c5621c82b5d94052cdaa13a571f05649
thuyduong7 <78083024+thuyduong7@users.noreply.github.com>
2021-02-21 19:42:49 +0700

    Create Bai1.cpp

---
+++ Bai1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0; i < n-1; i++){
+        for (int j = i+1; j < n; j++){
+            if (a[i] > a[j]) swap (a[i],a[j]);
+        }
+    }
+    bool same;
+    for (int i = 0; i < n-1; i++){
+        same = false;
+        for (int j = i+1; j < n; j++){
+            if (a[i] == a[j]){
+                same = true;
+                break;
+            }
+        }
+        if (same){
+            cout << "YES";
+            return 0;
+        }
+    }
+    cout << "NO";
+    return 0;
+}
+
